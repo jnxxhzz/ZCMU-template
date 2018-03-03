7e4b45302041b74c6b456d10810e10633791c757
jnxxhzz <33682882+jnxxhzz@users.noreply.github.com>
2018-03-03 12:08:47 +0800

    Add files via upload

---
+++ head.cpp
@@ -0,0 +1,77 @@
+/**
+*----------AC be here!----------/
+* 　　　┏┓　　　┏┓
+* 　　┏┛┻━━━┛┻┓
+* 　　┃　　　　　　　┃
+* 　　┃　　　━　　　┃
+* 　　┃　┳┛　┗┳　┃
+* 　　┃　　　　　　　┃
+* 　　┃　　　┻　　　┃
+* 　　┃　　　　　　　┃
+* 　　┗━┓　　　┏━┛
+* 　　　　┃　　　┃神兽保佑
+* 　　　　┃　　　┃代码无BUG！
+* 　　　　┃　　　┗━━━┓
+* 　　　　┃　　　　　　　┣┓
+* 　　　　┃　　　　　　　┏┛
+* 　　　　┗┓┓┏━┳┓┏┛
+* 　　　　　┃┫┫　┃┫┫
+* 　　　　　┗┻┛　┗┻┛
+* ━━━━神兽出没 ━━━━━by:XF*/
+//单身二十年的手速
+
+#pragma comment(linker, "/STACK:1024000000,1024000000")
+#include <bits/stdc++.h> //万能头文件 
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+using namespace std;
+
+#define rep(i,l,r) for( int i = l; i <= r; i++ )
+#define rep0(i,l,r) for( int i = l; i < r; i++ )
+#define ALL(v) (v).begin(),(v).end()
+#define cl(a,b) memset(a,b,sizeof(a))
+#define clr clear()
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+#define fin freopen("in.txt","r",stdin)
+#define fout freopen("out.txt","w",stdout)
+#define lson t<<1,l,mid
+#define rson t<<1|1,mid+1,r
+#define seglen(t) (node[t].r-node[t].l+1)
+#define lowbit(x) (x)&(-x)
+#define bp __builtin_popcount
+#define gcd(a,b) __gcd(a,b)
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef pair<int,int> PII;
+typedef pair<LL,LL> PLL;
+typedef vector<PII> VPII;
+const double PI = acos(-1);
+const double e = exp(1);
+const int INF = 0x3f3f3f3f;
+const int MOD = 1e9 + 7;
+
+
+
+
+
+
+
+
